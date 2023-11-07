8b1b668faff6360490badf478d4967702479780c
Vikram Singh Rana <116163333+vikramintech@users.noreply.github.com>
2023-11-07 16:39:02 +0000

    js code added

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 console.clear();
+
+// car object
+const cars ={
+    name: "Swift",
+    color: 4,
+    isSafe: false
+}
+// function that returns name propery of a given object
+function getName(object){
+    return object.name;
+}
+// invoking getName function
+console.log(getName(cars));
+
+
+// function take 3 numbers and give their product
+
+function getProduct(num1,num2,num3){
+    return num1*num2*num3;
+}
+
+// invoking getProduct function
+
+console.log(getProduct(2,4,7));
+
+// cars array
+const carBrands = ["audi","bmw","VW","skoda","bentely"]
+
+// function that takes an array and returns its fifth element
+
+function getFifthElement(array){
+    return array[4];
+}
+// invoking getfifthElement function
+
+console.log(getFifthElement(carBrands));
